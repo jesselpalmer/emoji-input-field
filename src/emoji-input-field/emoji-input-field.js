1e04299e933ae36f44333400bf76bf26d5f1c7c6
Jesse Palmer <jesselpalmer@gmail.com>
2019-07-22 21:34:49 -0700

    refactor: fix formatting

--- src/emoji-input-field/emoji-input-field.js
+++ src/emoji-input-field/emoji-input-field.js
@@ -1,34 +1,34 @@
 class EmojiInputBox extends HTMLElement {
   constructor() {
-    super();
+    super()
 
-    this.isColonEntered = false;
+    this.isColonEntered = false
 
-    const shadow = this.attachShadow({ mode: "open" });
+    const shadow = this.attachShadow({ mode: 'open' })
 
-    const input = document.createElement("input");
-    input.type = "text";
-    input.oninput = this.textChangeHandler;
+    const input = document.createElement('input')
+    input.type = 'text'
+    input.oninput = this.textChangeHandler
 
-    shadow.appendChild(input);
+    shadow.appendChild(input)
   }
 
   textChangeHandler(event) {
-    if (!this.isColonEntered && event.data === ":") {
-      this.isColonEntered = true;
-      console.log("start escaping");
-    } else if (this.isColonEntered && event.data === " ") {
-      this.isColonEntered = false;
-      console.log("not an emoji. exit escaping.");
-    } else if (this.isColonEntered && event.data === ":") {
-      console.log("exit escaping");
-      this.isColonEntered = false;
+    if (!this.isColonEntered && event.data === ':') {
+      this.isColonEntered = true
+      console.log('start escaping')
+    } else if (this.isColonEntered && event.data === ' ') {
+      this.isColonEntered = false
+      console.log('not an emoji. exit escaping.')
+    } else if (this.isColonEntered && event.data === ':') {
+      console.log('exit escaping')
+      this.isColonEntered = false
     } else if (this.isColonEntered) {
-      console.log("validating emoji text");
+      console.log('validating emoji text')
     }
 
-    console.log(this.isColonEntered);
+    console.log(this.isColonEntered)
   }
 }
 
-customElements.define("emoji-input-box", EmojiInputBox);
+customElements.define('emoji-input-box', EmojiInputBox)
