d5f6ada2bd3a1a93025fc180af9ea062d34a1992
Jesse Palmer <jesselpalmer@gmail.com>
2019-07-28 18:27:02 -0700

    feat: add test app

---
+++ test-app/emoji-input-field.js
@@ -0,0 +1,37 @@
+class EmojiInputField extends HTMLElement {
+  constructor() {
+    super()
+
+    this.isColonEntered = false
+
+    const shadow = this.attachShadow({ mode: 'open' })
+
+    const input = document.createElement('input')
+    input.type = 'text'
+    input.oninput = this.textChangeHandler
+
+    shadow.appendChild(input)
+  }
+
+  textChangeHandler(event) {
+    const EMOJI_ESCAPE_KEY = ':'
+    const BLANK_SPACE = ' '
+
+    if (!this.isColonEntered && event.data === EMOJI_ESCAPE_KEY) {
+      this.isColonEntered = true
+      console.log('start escaping')
+    } else if (this.isColonEntered && event.data === BLANK_SPACE) {
+      this.isColonEntered = false
+      console.log('not an emoji. exit escaping.')
+    } else if (this.isColonEntered && event.data === EMOJI_ESCAPE_KEY) {
+      console.log('exit escaping')
+      this.isColonEntered = false
+    } else if (this.isColonEntered) {
+      console.log('validating emoji text')
+    }
+
+    console.log(this.isColonEntered)
+  }
+}
+
+customElements.define('emoji-input-field', EmojiInputField)
