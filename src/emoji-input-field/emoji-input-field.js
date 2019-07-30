bfb37142d27f3a07483aa3d2a94d01aa72414b3a
Jesse Palmer <jesselpalmer@gmail.com>
2019-07-29 20:53:51 -0700

    refactor: create helper functions

--- src/emoji-input-field/emoji-input-field.js
+++ src/emoji-input-field/emoji-input-field.js
@@ -2,33 +2,51 @@ class EmojiInputField extends HTMLElement {
   constructor() {
     super()
 
-    this.isColonEntered = false
+    this.isTextEscaped = false
 
     const shadow = this.attachShadow({ mode: 'open' })
-
     const input = document.createElement('input')
+
     input.type = 'text'
     input.oninput = event => {
-      const EMOJI_ESCAPE_KEY = ':'
-      const BLANK_SPACE = ' '
+      const char = event.data
 
-      if (!this.isColonEntered && event.data === EMOJI_ESCAPE_KEY) {
-        this.isColonEntered = true
+      if (this.shouldTextEscapingStart(this.isTextEscaped, char)) {
+        this.isTextEscaped = true
         console.log('start escaping')
-      } else if (this.isColonEntered && event.data === BLANK_SPACE) {
-        this.isColonEntered = false
+      } else if (this.shouldTextEscapingStopBlankSpace(this.isTextEscaped, char)) {
+        this.isTextEscaped = false
         console.log('not an emoji. exit escaping.')
-      } else if (this.isColonEntered && event.data === EMOJI_ESCAPE_KEY) {
+      } else if (this.shouldTextEscapingStopClosingColon(this.isTextEscaped, char)) {
         console.log('exit escaping')
-        this.isColonEntered = false
-      } else if (this.isColonEntered) {
+        this.isTextEscaped = false
+      } else if (this.shouldSearchForEmoji(this.isTextEscaped)) {
         console.log('validating emoji text')
       }
 
-      console.log(this.isColonEntered)
+      console.log(this.isTextEscaped)
     }
 
     shadow.appendChild(input)
+  }
+
+  shouldTextEscapingStart(isTextEscaped, char) {
+    const EMOJI_ESCAPE_KEY = ':'
+    return !isTextEscaped && char === EMOJI_ESCAPE_KEY
+  }
+
+  shouldTextEscapingStopBlankSpace(isTextEscaped, char) {
+    const BLANK_SPACE = ' '
+    return isTextEscaped && char === BLANK_SPACE
+  }
+
+  shouldTextEscapingStopClosingColon(isTextEscaped, char) {
+    const EMOJI_ESCAPE_KEY = ':'
+    return isTextEscaped && char === EMOJI_ESCAPE_KEY
+  }
+
+  shouldSearchForEmoji(isTextEscaped) {
+    return isTextEscaped
   }
 }
 
--- test-app/emoji-input-field.js
+++ test-app/emoji-input-field.js
@@ -1,36 +1,54 @@
 class EmojiInputField extends HTMLElement {
-  
   constructor() {
     super()
 
-    this.isColonEntered = false
+    this.isTextEscaped = false
 
     const shadow = this.attachShadow({ mode: 'open' })
-
     const input = document.createElement('input')
+
     input.type = 'text'
-    input.oninput = (event) => {
-      const EMOJI_ESCAPE_KEY = ':'
-      const BLANK_SPACE = ' '
-  
-      if (!this.isColonEntered && event.data === EMOJI_ESCAPE_KEY) {
-        this.isColonEntered = true
+    input.oninput = event => {
+      const char = event.data
+      let isTextEscaped = this.isTextEscaped
+
+      if (this.shouldTextEscapingStart(isTextEscaped, char)) {
+        isTextEscaped = true
         console.log('start escaping')
-      } else if (this.isColonEntered && event.data === BLANK_SPACE) {
-        this.isColonEntered = false
+      } else if (this.shouldTextEscapingStopBlankSpace(isTextEscaped, char)) {
+        TextEscaped = false
         console.log('not an emoji. exit escaping.')
-      } else if (this.isColonEntered && event.data === EMOJI_ESCAPE_KEY) {
+      } else if (this.shouldTextEscapingStopClosingColon(isTextEscaped, char)) {
         console.log('exit escaping')
-        this.isColonEntered = false
-      } else if (this.isColonEntered) {
+        isTextEscaped = false
+      } else if (this.shouldSearchForEmoji(isTextEscaped)) {
         console.log('validating emoji text')
       }
-  
-      console.log(this.isColonEntered)
+
+      console.log(this.isTextEscaped)
     }
 
     shadow.appendChild(input)
-  } 
+  }
+
+  shouldTextEscapingStart(isTextEscaped, char) {
+    const EMOJI_ESCAPE_KEY = ':'
+    return !isTextEscaped && char === EMOJI_ESCAPE_KEY
+  }
+
+  shouldTextEscapingStopBlankSpace(isTextEscaped, char) {
+    const BLANK_SPACE = ' '
+    return isTextEscaped && char === BLANK_SPACE
+  }
+
+  shouldTextEscapingStopClosingColon(isTextEscaped, char) {
+    const EMOJI_ESCAPE_KEY = ':'
+    return isTextEscaped && char === EMOJI_ESCAPE_KEY
+  }
+
+  shouldSearchForEmoji(isTextEscaped) {
+    return isTextEscaped
+  }
 }
 
 customElements.define('emoji-input-field', EmojiInputField)
