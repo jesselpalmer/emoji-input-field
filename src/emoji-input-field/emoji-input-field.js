5724e64e06993a87a5f464aa730b1cba459874c0
Jesse Palmer <jesselpalmer@gmail.com>
2019-07-29 20:14:41 -0700

    fix: undefined issue

--- src/emoji-input-field/emoji-input-field.js
+++ src/emoji-input-field/emoji-input-field.js
@@ -8,29 +8,27 @@ class EmojiInputField extends HTMLElement {
 
     const input = document.createElement('input')
     input.type = 'text'
-    input.oninput = this.textChangeHandler
+    input.oninput = event => {
+      const EMOJI_ESCAPE_KEY = ':'
+      const BLANK_SPACE = ' '
 
-    shadow.appendChild(input)
-  }
+      if (!this.isColonEntered && event.data === EMOJI_ESCAPE_KEY) {
+        this.isColonEntered = true
+        console.log('start escaping')
+      } else if (this.isColonEntered && event.data === BLANK_SPACE) {
+        this.isColonEntered = false
+        console.log('not an emoji. exit escaping.')
+      } else if (this.isColonEntered && event.data === EMOJI_ESCAPE_KEY) {
+        console.log('exit escaping')
+        this.isColonEntered = false
+      } else if (this.isColonEntered) {
+        console.log('validating emoji text')
+      }
 
-  textChangeHandler(event) {
-    const EMOJI_ESCAPE_KEY = ':'
-    const BLANK_SPACE = ' '
-
-    if (!this.isColonEntered && event.data === EMOJI_ESCAPE_KEY) {
-      this.isColonEntered = true
-      console.log('start escaping')
-    } else if (this.isColonEntered && event.data === BLANK_SPACE) {
-      this.isColonEntered = false
-      console.log('not an emoji. exit escaping.')
-    } else if (this.isColonEntered && event.data === EMOJI_ESCAPE_KEY) {
-      console.log('exit escaping')
-      this.isColonEntered = false
-    } else if (this.isColonEntered) {
-      console.log('validating emoji text')
+      console.log(this.isColonEntered)
     }
 
-    console.log(this.isColonEntered)
+    shadow.appendChild(input)
   }
 }
 
--- test-app/emoji-input-field.js
+++ test-app/emoji-input-field.js
@@ -1,4 +1,5 @@
 class EmojiInputField extends HTMLElement {
+  
   constructor() {
     super()
 
@@ -8,30 +9,28 @@ class EmojiInputField extends HTMLElement {
 
     const input = document.createElement('input')
     input.type = 'text'
-    input.oninput = this.textChangeHandler
-
-    shadow.appendChild(input)
-  }
-
-  textChangeHandler(event) {
-    const EMOJI_ESCAPE_KEY = ':'
-    const BLANK_SPACE = ' '
-
-    if (!this.isColonEntered && event.data === EMOJI_ESCAPE_KEY) {
-      this.isColonEntered = true
-      console.log('start escaping')
-    } else if (this.isColonEntered && event.data === BLANK_SPACE) {
-      this.isColonEntered = false
-      console.log('not an emoji. exit escaping.')
-    } else if (this.isColonEntered && event.data === EMOJI_ESCAPE_KEY) {
-      console.log('exit escaping')
-      this.isColonEntered = false
-    } else if (this.isColonEntered) {
-      console.log('validating emoji text')
+    input.oninput = (event) => {
+      const EMOJI_ESCAPE_KEY = ':'
+      const BLANK_SPACE = ' '
+  
+      if (!this.isColonEntered && event.data === EMOJI_ESCAPE_KEY) {
+        this.isColonEntered = true
+        console.log('start escaping')
+      } else if (this.isColonEntered && event.data === BLANK_SPACE) {
+        this.isColonEntered = false
+        console.log('not an emoji. exit escaping.')
+      } else if (this.isColonEntered && event.data === EMOJI_ESCAPE_KEY) {
+        console.log('exit escaping')
+        this.isColonEntered = false
+      } else if (this.isColonEntered) {
+        console.log('validating emoji text')
+      }
+  
+      console.log(this.isColonEntered)
     }
 
-    console.log(this.isColonEntered)
-  }
+    shadow.appendChild(input)
+  } 
 }
 
 customElements.define('emoji-input-field', EmojiInputField)
